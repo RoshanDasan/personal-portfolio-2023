424213f2d4f5dd01e5df56c716db286113e03aef
RoshanDasan <edroshan2001@gmail.com>
2023-08-16 22:18:28 +0530

    mail sending complete

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,30 +1,57 @@
 const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log('submitted');
-    let name = document.getElementById('name').value
-    let email = document.getElementById('email').value
-    let subject = document.getElementById('subject').value
-    let flag = 0
+  e.preventDefault();
+  console.log("submitted");
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let subject = document.getElementById("subject").value;
+  let message = document.getElementById("message").value;
+  let flag = 0;
 
-    if(name.length == 0 || /^\S*$/.test(name) == false){
-        document.getElementById('nameError').innerHTML = 'Please Enter Valid Name'
-        flag = 1
-    }
-    if(email.length == 0 || /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email) == false){
-        document.getElementById('emailError').innerHTML = 'Please Enter Valid Email'
-        flag = 1
-    }
-    if(subject.length == 0 || /^\S*$/.test(subject) == false){
-        document.getElementById('subjectError').innerHTML = 'Please Enter Valid Subject'
-        flag = 1
-    }
+  if (name.length == 0 || /^\S*$/.test(name) == false) {
+    document.getElementById("nameError").innerHTML = "Please Enter Valid Name";
+    flag = 1;
+  }
+  if (
+    email.length == 0 ||
+    /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(email) == false
+  ) {
+    document.getElementById("emailError").innerHTML =
+      "Please Enter Valid Email";
+    flag = 1;
+  }
+  if (subject.length == 0 || /^\S*$/.test(subject) == false) {
+    document.getElementById("subjectError").innerHTML =
+      "Please Enter Valid Subject";
+    flag = 1;
+  }
 
-    if(flag == 0){
-        const errorSpans = document.querySelectorAll("#nameError, #emailError, #subjectError");
-        errorSpans.forEach((span)=>{
-            span.innerHTML = ""
-        })
-        
-        
-    }
-}
+  if (flag == 0) {
+    const errorSpans = document.querySelectorAll(
+      "#nameError, #emailError, #subjectError"
+    );
+    errorSpans.forEach((span) => {
+      span.innerHTML = "";
+    });
+
+
+    fetch("http://localhost:5000/send-mail", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name, email, subject, message }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json(); // Assuming the response is JSON
+      })
+      .then((data) => {
+        console.log(data); // This will log the response data
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+      });
+  }
+};
--- server.js
+++ server.js
@@ -1,5 +1,39 @@
-const express = require('express')
-const app = express()
+const bodyParser = require("body-parser");
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+require("dotenv").config()
+const app = express();
 
 
-app.listen(5000,()=> console.log('server started'))
+app.use(cors());
+app.use(bodyParser.json());
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL ,
+    pass: process.env.PASSWORD ,
+  },
+});
+
+app.post("/send-mail", (req, res) => {
+  const { name, email, subject, message } = req.body;
+  const mailOptions = {
+    from: email,
+    to: "edroshan2001@gmail.com",
+    subject,
+    text: message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      res.status(500).send(`error generating email ${error}`);
+    } else {
+      res.status(200).send(`email sending success ${info}`);
+    }
+  });
+});
+
+app.listen(5000, () => console.log("server started")); 
